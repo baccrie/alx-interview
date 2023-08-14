8b6c1b93fb54f748fa981e903171b76f6f3cd978
baccrie <bakarerilwan1@gmail.com>
2023-08-14 21:39:09 +0100

    Task 0 done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-"""
-    This module contains the canUnlockAll function for the interview prep.
-"""
 
+"""A FUnction that performs wahala"""
 
 def canUnlockAll(boxes):
-    """A Method that determines if all the boxes can be opened.
-        Arg:
-            boxes: Is a list of lists.
-    """
     box = len(boxes) * [False]
     box[0] = True
     keys = [0]
 
-    for each in keys:
-        for i in boxes[each]:
+    for key in keys:
+        for i in boxes[key]:
             if i not in keys:
-                if i < len(boxes):
-                    box[i] = True
-                    keys.append(i)
-    return all(box)
+                box[i] = True
+                keys.append(i)
+
+    for val in box:
+        if not val:
+            return False
+
+    return True
